4fa1ebc68ddade665e891b3f1df2a4be1501635b
Jorge Hernán <jorge.hernan@radmas.com>
2013-09-25 15:13:45 +0200

    Documentación

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 OpenERPCrmExtension
 ===================
+
+Este módulo extiende la funcionalidad base del módulo de CRM de OpenERP.
+        
+        Se han añadido las siguientes funcionalidades:
+        
+        Categorías: 
+        - Añadido campo Padre para poder anidar las categorías. Este campo aparece en todas las vistas, y se permite filtrado y agrupación
+        - Añadida vista jerárquica de categorías, donde se puede visualizar todo el árbol de categorías.
+        
+        Iniciativas:
+        - Se ha añadido el campo Categoría al listado.
+        - El nombre que se muestra de la Categoría corresponde a la estructura jerárquica de la categoría (Padre / Hijo).
--- crm_case_ext/__openerp__.py
+++ crm_case_ext/__openerp__.py
@@ -28,7 +28,17 @@
     "author": "Radmas Technologies S.L.",
     "category": "CRM",
     "description": """
+        Este módulo extiende la funcionalidad base del módulo de CRM de OpenERP.
         
+        Se han añadido las siguientes funcionalidades:
+        
+        Categorías: 
+        - Añadido campo Padre para poder anidar las categorías. Este campo aparece en todas las vistas, y se permite filtrado y agrupación
+        - Añadida vista jerárquica de categorías, donde se puede visualizar todo el árbol de categorías.
+        
+        Iniciativas:
+        - Se ha añadido el campo Categoría al listado.
+        - El nombre que se muestra de la Categoría corresponde a la estructura jerárquica de la categoría (Padre / Hijo).
     """,
     "init_xml": [],
     'update_xml': ['crm_case_categ_ext_view.xml',
